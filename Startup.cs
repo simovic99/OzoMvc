613d5d77e608af805b22bc17fe6a9b5c4a494b69
simovic99 <simovic.petar99@gmail.com>
2020-05-26 16:32:56 +0200

    sakrivena lozinka

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,12 +14,22 @@ namespace OzoMvc
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            //services.Add
+            services.AddDbContext<Models.PI05Context>(options =>
+                                                    options.UseSqlServer(
+                                                              Configuration.GetConnectionString("Ozo")
+                                                                           .Replace("lozinka", Configuration["OzoSqlPassword"])
+                                                              ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
